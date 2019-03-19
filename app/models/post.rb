d4a6ecf1d71fae313824fca310cc3ffbfe3833a4
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-19 17:47:49 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,9 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
-  def categories_attributes=(category_attributes)	
-    category_attributes.values.each do |category_attribute|	
-      category = Category.find_or_create_by(category_attribute)	
-      self.categories << category	
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
     end
 end
